698e862f38f763fccdfd80c48ffb26f51c452de0
datduyng <35666615+datduyng@users.noreply.github.com>
2022-01-19 09:08:50 +0000

    Deploying to gh-pages from  @ f6d6013f292388f42d746636caf21ba5cfb83730 🚀

---
+++ _next/static/coBTZ5jnUzsEYAuA6w8tl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-796817d4ad47d903.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/home":["static/chunks/pages/home-7e0803cf523305f8.js"],sortedPages:["/","/_app","/_error","/home"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
