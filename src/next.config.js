f2295b5070656dc5881ba9ec277e8421411ff6ec
datduyng <datnguyen5653@gmail.com>
2022-01-19 00:38:54 -0800

    test

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/datduyng/datduyng.github.io/gh-pages/' : '',
 }
