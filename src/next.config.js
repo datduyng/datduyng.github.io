3da4cfcf27367e85e017991574552c433e3daf7f
datduyng <datnguyen5653@gmail.com>
2022-01-19 00:59:02 -0800

    remove next config

--- src/next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  reactStrictMode: true,
-    // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/datduyng/datduyng.github.io/gh-pages/' : '',
-}
