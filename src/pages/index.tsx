77928287e176df4dd2f3dc51462f0e1af53b3a22
datduyng <datnguyen5653@gmail.com>
2022-01-19 00:36:32 -0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+      <h1>
+        testing
+      </h1>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
