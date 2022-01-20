53309f5e3d8be10a942ee93f047a15caff78addd
datduyng <datnguyen5653@gmail.com>
2022-01-20 13:03:55 -0800

    refactor link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,198 +1,182 @@
-import React, { createRef, useEffect, useState } from "react";
-import { FaHeart } from "react-icons/fa";
-import { VscChromeClose } from "react-icons/vsc";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { Welcome } from "./match";
-import { Oops } from "./oops";
+import React, { useEffect, useState } from "react";
 import {
-  AnimatePresence,
-  motion,
-  useAnimation,
-  useMotionValue,
-  useTransform,
-} from "framer-motion";
+  FaLinkedin,
+  FaGithub,
+  FaRegFilePdf,
+  FaProductHunt,
+} from "react-icons/fa";
 import Layout from "../components/layout";
-
-const SWIPE_THRESHOLD = 60;
-const Home = () => {
-  const cardSwipeRef = createRef<any>();
-  const router = useRouter();
-  const swipe = async (dir: "left" | "right") => {
-    if (cardSwipeRef.current?.swipe) {
-      await cardSwipeRef.current.swipe(dir);
-    }
-
-    if (dir === "left") {
-      router.push("/oops");
-    } else {
-      router.push("/match");
-    }
-  };
-
-  // const [swipedState, setSwipedState] = useState<"left" | "current" | "right">(
-  //   "current"
-  // );
-
-  // move the card as user drag the cursor
-  const motionValue = useMotionValue(0 as any);
-  // to rotate the card as the card move on drag
-  const rotateValue = useTransform<any, any>(
-    motionValue,
-    [-150, 150],
-    [-30, 30]
-  );
-
-  // To decrease opacity of the card when swiped
-  // on dragging card to left(-200) or right(200)
-  // opacity gradually changes to 0
-  // and when the card is in center opacity = 1
-  const opacityValue = useTransform(
-    motionValue,
-    [-100, -50, 0, 50, 100],
-    [0, 1, 1, 1, 0]
-  );
-
-  const tinderCard = (
-    <div className="bg-white border-2 rounded-md">
-      <div
-        style={{
-          overflow: "hidden",
-          pointerEvents: "none",
-        }}
-      >
-        <img
-          src="/personal.jpg"
-          className="rounded-t-md"
-          style={{
-            height: "415px",
-            width: "405px",
-          }}
-        />
+type ButtonType = {
+  color: string;
+  name: string;
+  icon: React.ReactNode;
+  href?: string;
+  newTab: boolean;
+};
+const links: ButtonType[] = [
+  {
+    name: "My Resume",
+    color: "#00ab6c",
+    icon: <FaRegFilePdf fontSize={20} color={"#fff"} />,
+    href: "https://drive.google.com/file/d/1SPiRTXqf17ZNlhvNR6tgZHnR4bgF9IpS/view",
+    newTab: true,
+  },
+  {
+    name: "Product Gallery",
+    color: "#1ab7ea",
+    icon: <FaProductHunt fontSize={20} color={"#fff"} />,
+    href: "/products",
+    newTab: false,
+  },
+  {
+    name: "LinkedIn",
+    color: "#2867B2",
+    icon: <FaLinkedin fontSize={20} />,
+    href: "https://www.linkedin.com/in/datdnguyen/",
+    newTab: true,
+  },
+  {
+    name: "Github",
+    color: "#6e5494",
+    icon: <FaGithub fontSize={20} />,
+    href: "https://www.github.com/datduyng/",
+    newTab: true,
+  },
+];
+const ButtonTemplate = ({
+  color,
+  name,
+  icon,
+  href,
+  newTab = false,
+}: ButtonType) => {
+  const buttonContent = (
+    <button
+      style={{
+        backgroundColor: color,
+      }}
+      className={`hover-scale h-14 w-full 
+      shadow-lg rounded-md
+      text-white font-semibold 
+      hover:opacity-75 place-content-center`}
+    >
+      <div className="flex justify-center">
+        {icon}
+        <span className="ml-2">{name}</span>
       </div>
-      <div className="p-3">
-        <p className="text-xl">Dominic Nguyen</p>
-        <p className="text-base text-slate-400">@datduyng</p>
-        <p className="pb-3">Just vib'in. ❤️ Open source.</p>
-        <hr />
-        <div className="text-sm pt-3">
-          he/him, 5'9. I like to work out 🏋️‍♂️ rock climbing 🧗 skiing ⛷️ running
-          🏃‍♂️ and building products 🕹️. I like to blog on IOTs, web tech, infra
-          related. Tinkering with the web 3.0. Swipe right for my resume 😜
+    </button>
+  );
+  return (
+    <div>
+      {newTab ? (
+        <a target="_blank" href={href}>
+          {buttonContent}
+        </a>
+      ) : (
+        <Link href={href || "/"}>{buttonContent}</Link>
+      )}
+    </div>
+  );
+};
+export function Welcome() {
+  const router = useRouter();
+  const [itsaMatch, setItsaMatch] = useState<boolean | null>(null);
+  useEffect(() => {
+    if (router.isReady) {
+
+      setItsaMatch(router.query.match !== undefined);
+    }
+  }, [router.isReady]);
+
+  return (
+    <div
+      className="flex flex-col mx-5 mt-10"
+      style={{
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {itsaMatch === null ? (
+        <div className="h-72"></div>
+      ) : itsaMatch ? (
+        <div
+          className="flex flex-col justify-center items-center
+        text-center"
+        >
+          <div className="text-red-600 mtext text-6xl mt-10 ">
+            It's a match!
+          </div>
+          <div className="flex flex-row gap-10 mt-5">
+            <div
+              style={{
+                backgroundColor: '#e17055',
+                backgroundSize: "contain",
+                minHeight: "150px",
+                minWidth: "150px",
+                borderRadius: '50%'
+              }}
+            />
+            <img
+              src="/avatar.png"
+              style={{
+                backgroundSize: "contain",
+                height: "150px",
+                width: "150px",
+              }}
+              alt="Dominic Nguyen"
+            />
+          </div>
+          <div className="text-base font-semibold mt-5">
+            You can now connect with Dom on LinkedIn and Github!!
+            </div>
         </div>
+      ) : (
+        <div
+          className="sm:w-full md:w-3/4 xl:w-3/4 
+        flex flex-col justify-center items-center
+        text-center"
+        >
+          <img
+            src="/avatar.png"
+            style={{
+              backgroundSize: "contain",
+              height: "300px",
+              width: "300px",
+            }}
+            alt="Dominic Nguyen"
+          />
+          <div className="text-4xl font-semibold mt-6">Dominic Nguyen</div>
+          <hr className="mt-2" />
+          <div className="mt-2">
+            Howdy, I'm Dom. I'm a passionate Tech Lover from Seattle, WA. Apart
+            from that, I'm a software engineer and an opensource enthusiast
+            inspired by imagination
+          </div>
+        </div>
+      )}
+
+      <div className="w-full mt-5 flex flex-col gap-6">
+        {links.map((link) => (
+          <ButtonTemplate
+            name={link.name}
+            icon={link.icon}
+            color={link.color}
+            href={link.href}
+            newTab={link.newTab}
+          />
+        ))}
       </div>
     </div>
   );
-  const animControls = useAnimation();
-  const [swipedState, setSwipeState] = useState<"left" | "mid" | "right">(
-    "mid"
-  );
+}
 
-  // useEffect(() => {
-  //   motionValue.onChange(curr => {
-  //     if
-  //   })
-  // })
-  const homeComponents = (
-    <motion.div
-      style={{
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignContent: "center",
-          alignItems: "center",
-          maxWidth: "405px",
-          margin: "auto",
-        }}
-      >
-        <div className="text-3xl pb-5 xs:hidden sm:hidden md:flex">
-          Meet Dom!!
-        </div>
-        <motion.div
-          drag
-          style={{ x: motionValue, rotate: rotateValue, opacity: opacityValue }}
-          animate={animControls}
-          custom={motionValue}
-          // initial={{ scale: 0.9 }}
-          exit={(motionValue.get() * 10) as any}
-          onDragEnd={(event, info) => {
-            if (Math.abs(motionValue.get()) > SWIPE_THRESHOLD) {
-              // remove();
-              console.log("remove info", event, info, motionValue.get());
-              setSwipeState(motionValue.get() > 0 ? "right" : "left");
-            } else {
-              animControls.start({ x: 0, y: 0 });
-            }
-          }}
-          // exit={(x) => ({ x: x.get() * 10 as any })}
-        >
-          {tinderCard}
-        </motion.div>
-        <div className="flex justify-between">
-          <button
-            className="bg-white m-4"
-            style={{
-              height: "60px",
-              width: "60px",
-              borderRadius: "50%",
-            }}
-            onClick={() => swipe("left")}
-          >
-            <VscChromeClose
-              fontSize={32}
-              style={{
-                margin: "auto",
-              }}
-              color="#CDD6DD"
-            />
-          </button>
-          <button
-            className="bg-white m-4"
-            style={{
-              height: "60px",
-              width: "60px",
-              borderRadius: "50%",
-            }}
-            onClick={() => {
-              swipe("right");
-            }}
-          >
-            <FaHeart
-              fontSize={32}
-              style={{
-                margin: "auto",
-              }}
-              color="#FFACE4"
-            />{" "}
-          </button>
-        </div>
-      </div>
-    </motion.div>
-  );
-
-  switch (swipedState) {
-    case "mid":
-      return <AnimatePresence>{homeComponents}</AnimatePresence>;
-    case "left":
-      return <Oops />;
-    case "right":
-      return <Welcome />;
-    default:
-      return <></>;
-      break;
-  }
-};
-
-const HomeWrapper = () => {
+export default function WelcomeWrap() {
   return (
     <Layout>
-      <Home />
+      <Welcome />
     </Layout>
   );
-};
-
-export default HomeWrapper;
+}
---
+++ src/pages/tinr.tsx
@@ -0,0 +1,204 @@
+import React, { createRef, useEffect, useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { VscChromeClose } from "react-icons/vsc";
+import { useRouter } from "next/router";
+import { Welcome } from ".";
+import { Oops } from "./oops";
+import {
+  AnimatePresence,
+  motion,
+  useAnimation,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import Layout from "../components/layout";
+
+const SWIPE_THRESHOLD = 60;
+const Home = () => {
+  const cardSwipeRef = createRef<any>();
+  const router = useRouter();
+  const swipe = async (dir: "left" | "right") => {
+    if (cardSwipeRef.current?.swipe) {
+      await cardSwipeRef.current.swipe(dir);
+    }
+
+    if (dir === "left") {
+      router.push("/oops");
+    } else {
+      router.push("/?match");
+    }
+  };
+
+  // const [swipedState, setSwipedState] = useState<"left" | "current" | "right">(
+  //   "current"
+  // );
+
+  // move the card as user drag the cursor
+  const motionValue = useMotionValue(0 as any);
+  // to rotate the card as the card move on drag
+  const rotateValue = useTransform<any, any>(
+    motionValue,
+    [-150, 150],
+    [-30, 30]
+  );
+
+  // To decrease opacity of the card when swiped
+  // on dragging card to left(-200) or right(200)
+  // opacity gradually changes to 0
+  // and when the card is in center opacity = 1
+  const opacityValue = useTransform(
+    motionValue,
+    [-100, -50, 0, 50, 100],
+    [0, 1, 1, 1, 0]
+  );
+
+  const tinderCard = (
+    <div className="bg-white border-2 rounded-md">
+      <div
+        style={{
+          overflow: "hidden",
+          pointerEvents: "none",
+        }}
+      >
+        <img
+          src="/personal.jpg"
+          className="rounded-t-md"
+          style={{
+            height: "415px",
+            width: "405px",
+          }}
+        />
+      </div>
+      <div className="p-3">
+        <p className="text-xl">Dominic Nguyen</p>
+        <p className="text-base text-slate-400">@datduyng</p>
+        <p className="pb-3">Just vib'in. ❤️ Open source.</p>
+        <hr />
+        <div className="text-sm pt-3">
+          he/him, 5'9. I like to work out 🏋️‍♂️ rock climbing 🧗 skiing ⛷️ running
+          🏃‍♂️ and building products 🕹️. I like to blog on IOTs, web tech, infra
+          related. Tinkering with the web 3.0. Swipe right for my resume 😜
+        </div>
+      </div>
+    </div>
+  );
+  const animControls = useAnimation();
+  const [swipedState, setSwipeState] = useState<"left" | "mid" | "right">(
+    "mid"
+  );
+
+  // useEffect(() => {
+  //   motionValue.onChange(curr => {
+  //     if
+  //   })
+  // })
+  const homeComponents = (
+    <motion.div
+      style={{
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          maxWidth: "405px",
+          margin: "auto",
+        }}
+      >
+        <div className="text-3xl pb-5 xs:hidden sm:hidden md:flex">
+          Meet Dom!!
+        </div>
+        <motion.div
+          drag
+          style={{ x: motionValue, rotate: rotateValue, opacity: opacityValue }}
+          animate={animControls}
+          custom={motionValue}
+          // initial={{ scale: 0.9 }}
+          exit={(motionValue.get() * 10) as any}
+          onDragEnd={(event, info) => {
+            if (Math.abs(motionValue.get()) > SWIPE_THRESHOLD) {
+              // remove();
+              console.log("remove info", event, info, motionValue.get());
+              const dir = motionValue.get() > 0 ? "right" : "left";
+              setSwipeState(dir);
+              if (dir ==="left") {
+                router.push('/oops');
+              } else {
+                router.push('/?match')
+              }
+            } else {
+              animControls.start({ x: 0, y: 0 });
+            }
+          }}
+          // exit={(x) => ({ x: x.get() * 10 as any })}
+        >
+          {tinderCard}
+        </motion.div>
+        <div className="flex justify-between">
+          <button
+            className="bg-white m-4"
+            style={{
+              height: "60px",
+              width: "60px",
+              borderRadius: "50%",
+            }}
+            onClick={() => swipe("left")}
+          >
+            <VscChromeClose
+              fontSize={32}
+              style={{
+                margin: "auto",
+              }}
+              color="#CDD6DD"
+            />
+          </button>
+          <button
+            className="bg-white m-4"
+            style={{
+              height: "60px",
+              width: "60px",
+              borderRadius: "50%",
+            }}
+            onClick={() => {
+              swipe("right");
+            }}
+          >
+            <FaHeart
+              fontSize={32}
+              style={{
+                margin: "auto",
+              }}
+              color="#FFACE4"
+            />{" "}
+          </button>
+        </div>
+      </div>
+    </motion.div>
+  );
+
+  switch (swipedState) {
+    case "mid":
+      return <AnimatePresence>{homeComponents}</AnimatePresence>;
+    case "left":
+      return <Oops />;
+    case "right":
+      return <Welcome />;
+    default:
+      return <></>;
+      break;
+  }
+};
+
+const HomeWrapper = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default HomeWrapper;
